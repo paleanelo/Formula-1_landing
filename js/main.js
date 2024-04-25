13fb9e16d55702c7da734681295d0075a63ee439
paleanelo <168094734+paleanelo@users.noreply.github.com>
2024-04-25 16:32:58 +0300

    Delete js directory

--- js/main.js
+++
@@ -1,6 +0,0 @@
-const menuBtn = document.querySelector('.menu_btn');
-const menu = document.querySelector('.menu_list');
-
-menuBtn.addEventListener('click', () => {
-    menu.classList.toggle('menu_list-active');
-});
